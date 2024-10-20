84be85045c9b033409a2672ebb00d4a4c926850f
shivd236 <146203595+shivd236@users.noreply.github.com>
2024-10-20 10:24:15 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json({
 }))
 
 app.use(cors({
-  origin: 'https://hilarious-pika-529cc3.netlify.app', // Your frontend URL
+  origin: 'https://crudmernproject.netlify.app', // Your frontend URL
   credentials: true 
 }));
 
